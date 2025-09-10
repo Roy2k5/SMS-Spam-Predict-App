88b20057ad1907366597e59e17fc2dba941f18b2
Roy2k5 <tiendat3105vn@gmail.com>
2025-09-10 09:52:20 +0700

    fix: space amongs lines

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-import streamlit as st
 import pickle
+
+import streamlit as st
+
 st.title("SMS Spam Classifier")
 st.write("This app classifies SMS messages as spam or ham (not spam).")
 id2label = {0: "ham", 1: "spam"}
 import re
+
+
 def preprocess_text(text):
     # Example preprocessing: lowercasing and removing punctuation
-    text = text.replace('\n', ' ').strip()
-    text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
-    text = re.sub(r'\s+', ' ', text)  # Remove extra whitespace
-    text = re.sub(r'\d+', '', text)  # Remove digits
+    text = text.replace("\n", " ").strip()
+    text = re.sub(r"[^\w\s]", "", text)  # Remove punctuation
+    text = re.sub(r"\s+", " ", text)  # Remove extra whitespace
+    text = re.sub(r"\d+", "", text)  # Remove digits
     return text.lower()
+
+
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 with open("vectorizer.pkl", "rb") as f:
